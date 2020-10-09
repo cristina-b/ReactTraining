6919b28a4d98e73dc64e8d9227f89b802cfb4c58
Cristina Budinschi <cristina.budinschi@webbeds.com>
2020-10-09 11:40:47 +0300

    Day1

--- Recap/Exercise2.js
+++ Recap/Exercise2.js
@@ -2,11 +2,44 @@ const add = (a,b) => a+b;
 const subtract = (a,b) => a-b;
 const double = a => a*2;
 
-function chain()
+function chain(functions) 
 {
+    class Chain 
+    {
+        constructor(val) 
+        {
+            this.execute = () => val;
+        }
+    }
 
+    for(let fn in functions)
+    {
+        Chain.prototype[fn] = function (a, b) 
+        {
+            let currentValue = this.execute();
+            if (currentValue === null) 
+            {
+                args = [a, b]  //first function
+            }
+            else
+            {
+                args = [value, a]; //adding new parameters
+            }             
+            return new Chain(functions[fn](...args));
+        }
+
+        Chain.prototype.sum = function(a,b)
+        {
+            return a+b;
+        }
+    }
+    return new Chain(null);
 }
 
-const c = chain({add, substract, double});
-c.sum(5,3).double().subtract(10).execute();
-c.subtract(5,10).double().double().add(22);
+const c = chain({add, subtract, double});
+
+let result = c.sum(5,3).double().subtract(10).execute();
+console.log(result);
+
+let result2 = c.subtract(5,10).double().double().sum(22).execute();;
+console.log(result2);
