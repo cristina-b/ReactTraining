bc48aac0fd67dd5b86617781adbe1fc12e1df5f8
Cristina Budinschi <cristina.budinschi@webbeds.com>
2020-11-04 12:04:34 +0200

    a few tests

---
+++ react-app/src/components/Signup/Signup.test.js
@@ -0,0 +1,41 @@
+// dependencies
+import React from 'react';
+import { render, fireEvent, screen, getByDisplayValue } from "@testing-library/react";
+
+// components
+import Signup from './signup';
+
+describe('<Signup />', () => {
+
+
+
+    it('should have a `<form>` element', () => {
+        const { getByText } = render(<Signup />);
+        const form = getByText(/form/i);
+        expect(form).toBeInTheDocument();
+    });
+
+
+    it("Empty input validations are working ", () => {
+
+        const mockSubmit = jest.fn();
+        const { queryByTestId } = render(<Signup handleSubmit={mockSubmit} />);
+
+        fireEvent.change(queryByTestId("input-firstName"), { target: { value: 'Joe' } });
+        fireEvent.change(queryByTestId("input-lastName"), { target: { value: 'Doe' } });
+        fireEvent.change(queryByTestId("input-email"), { target: { value: 'john@doe.com' } });
+
+        fireEvent.submit(queryByTestId("input-form"));
+
+        const { getByText } = render(<Signup />);
+        const passwordError = getByText('Password is required');
+        expect(passwordError).toBeInTheDocument();
+
+        const termsError = getByText('Please check terms and conditions');
+        expect(termsError).toBeInTheDocument();  
+        
+        //const emailError = getByText('Email is required');
+        //expect(emailError).toThrow();  
+        //expect(mockSubmit).toHaveBeenCalled(); //   
+    });
+});
